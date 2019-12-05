dd2edbdd28edb7a9fff4e6e463a110e14a9eb3be
baitu.huang <baitu.huang@shopee.com>
2019-12-05 15:36:33 +0800

    fix style

--- sw-1.0.3.js
+++ sw-1.0.3.js
@@ -1,5 +1,5 @@
 
-  const filesToCache = ["/","favicon.84292e5e.ico","icon.585561b6.png","icon.png","index.html","main.4d1e485b.js","main.b80b9f6c.css","site.webmanifest","vendor.36cb5d1f.css","vendor.cfaaef55.js"];
+  const filesToCache = ["/","favicon.84292e5e.ico","icon.585561b6.png","icon.png","index.html","main.625b89d0.css","main.939fb288.js","site.webmanifest","vendor.36cb5d1f.css","vendor.cfaaef55.js"];
   
   const staticCacheName = "1.0.3";
   
