6380ea169fb835b6145f3dd98e7e5ccd170dadce
baitu.huang <baitu.huang@shopee.com>
2019-12-23 17:55:28 +0800

    use immer and neverland

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 
-const filesToCache = ["/zip-files/?source=pwa","404.html","LICENSE.txt","favicon.84292e5e.ico","favicon.ico","humans.txt","icon.585561b6.png","icons/icon-128x128.png","icons/icon-144x144.png","icons/icon-152x152.png","icons/icon-192x192.png","icons/icon-384x384.png","icons/icon-512x512.png","icons/icon-72x72.png","icons/icon-96x96.png","icons/icon.png","index.html","main.1183e780.css","main.b17d4583.js","robots.txt","site.webmanifest","vendor.36cb5d1f.css","vendor.746d9cb7.js"]
+const filesToCache = ["/zip-files/?source=pwa","404.html","LICENSE.txt","favicon.84292e5e.ico","favicon.ico","humans.txt","icon.585561b6.png","icons/icon-128x128.png","icons/icon-144x144.png","icons/icon-152x152.png","icons/icon-192x192.png","icons/icon-384x384.png","icons/icon-512x512.png","icons/icon-72x72.png","icons/icon-96x96.png","icons/icon.png","index.html","main.04125673.css","main.c08019f6.js","robots.txt","site.webmanifest","vendor.36cb5d1f.css","vendor.923ef56d.js"]
 
 const staticCacheName = 'zip-files_' + "1.0.4"
 
